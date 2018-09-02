9716a87fd190b0795d775f6e4a31793e3b0d2139
viviclara <viviclara@192.168.178.68>
2018-09-02 12:38:00 +0200

    teste alle Methoden von Musikstueck

--- MusikDB/src/test/TestMusikstueck.java
+++ MusikDB/src/test/TestMusikstueck.java
@@ -14,5 +14,26 @@ class TestMusikstueck {
 		musikstueck1.setTitel("Hallo Nacht");
 		assertEquals(musikstueck1.getTitel(), "Hallo Nacht");
 	}
+	
+	@Test
+	void testInterpret() {
+		Musikstueck musikstueck1 = new Musikstueck();
+		musikstueck1.setInterpret("Kraftklub");
+		assertEquals(musikstueck1.getInterpret(), "Kraftklub");
+	}
+	
+	@Test
+	void testGenre() {
+		Musikstueck musikstueck1 = new Musikstueck();
+		musikstueck1.setGenre("Rock Pop");
+		assertEquals(musikstueck1.getGenre(), "Rock Pop");
+	}
+	
+	@Test
+	void testPfad() {
+		Musikstueck musikstueck1 = new Musikstueck();
+		musikstueck1.setPfad("Bester Pfad evaaa");
+		assertEquals(musikstueck1.getPfad(), "Bester Pfad evaaa");
+	}
 
 }
